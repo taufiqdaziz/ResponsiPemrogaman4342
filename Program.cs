53ee9d147661154df0399e216f9d2084e3b63b78
taufiqdaziz <taufiqdaziz@students.amikom.ac.id>
2022-04-19 11:59:46 +0700

    responsi

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsi4342
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            karyawan Karyawan = new karyawan();
+
+
+            Console.WriteLine("Nik = ") ; Karyawan.nik = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nama = "); Karyawan.nama = Console.ReadLine();
+            Console.WriteLine("jumlah gaji = ") ; Karyawan.gaji = Convert.ToInt32(Console.ReadLine());
+
+            Karyawan.Nama();
+            Karyawan.Nik();
+            Karyawan.GajiBulanan();
+
+            Console.WriteLine("naik gaji 10%");
+
+            Karyawan.Nama();
+            Karyawan.Nik();
+            Karyawan.GajiBulananNaik();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ karyawan.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsi4342
+{
+    class karyawan
+    {
+        public int nik { get; set; }
+        public string nama { get; set; }
+        public double gaji { get; set; }
+
+        public void Nik()
+        {
+            Console.WriteLine("NIK = {0}", Convert.ToInt32(nik));
+        }
+
+        public void Nama()
+        {
+            Console.WriteLine("Nama = {0}", nama);
+        }
+
+        public void GajiBulanan()
+        {
+            if(gaji <= 0)
+            {
+                gaji = 0;
+                Console.WriteLine("Gaji = {0}", Convert.ToInt32(gaji));
+            }
+            else
+            {
+                Console.WriteLine("Gaji = {0}", Convert.ToInt32(gaji));
+            }
+            
+        }
+
+        public void GajiBulananNaik()
+        {
+            gaji = (gaji * 0.1) + gaji;
+            Console.WriteLine("Gaji = {0}", gaji);
+        }
+    }
+}
